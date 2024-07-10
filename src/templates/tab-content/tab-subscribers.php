41d425a05e57d707ee41392b12d71c88d6c28da8
Mark Nokes <mark.nokes@users.noreply.github.com>
2024-07-10 17:38:39 -0500

    fix indentation

--- src/templates/tab-content/tab-subscribers.php
+++ src/templates/tab-content/tab-subscribers.php
@@ -6,37 +6,37 @@
 
 if($data = $this->subscriber_table_options_page()) {
 
-        if($data['num_subs']) {
+	if($data['num_subs']) {
 
-        	?>
-        	
-        	<form id="subs-search">
-				<input type="email" id="email-input" placeholder="Search by email address" />
-				<?php wp_nonce_field('search_by_email', 'search_by_email'); ?>
-	        	<input type="submit" name="search" value="Search">
-	        </form>
-			<br />
-        	
-        	<?php
+		?>
+		
+		<form id="subs-search">
+			<input type="email" id="email-input" placeholder="Search by email address" />
+			<?php wp_nonce_field('search_by_email', 'search_by_email'); ?>
+			<input type="submit" name="search" value="Search">
+		</form>
+		<br />
+		
+		<?php
 
-        	echo wp_kses_post($data['html']);
+		echo wp_kses_post($data['html']);
 
-			$export_url = add_query_arg([
-			    'ppsfwoo_export_table'  => 1,
-			    '_wpnonce' 		=> wp_create_nonce('db_export_nonce')
-			], admin_url('admin.php?page=subscriptions_for_woo'));
-			
-			?>
+		$export_url = add_query_arg([
+			'ppsfwoo_export_table'  => 1,
+			'_wpnonce' => wp_create_nonce('db_export_nonce')
+		], admin_url('admin.php?page=subscriptions_for_woo'));
+		
+		?>
 
-			<a class="button export-table-data" href="<?php echo esc_url($export_url); ?>" target="_blank">Export Table Data</a>
-	        
-	        <?php 
+		<a class="button export-table-data" href="<?php echo esc_url($export_url); ?>" target="_blank">Export Table Data</a>
+		
+		<?php 
 
-    	} else if(0 === $data['num_subs']) {
+	} else if(0 === $data['num_subs']) {
 
-    		echo "<p>When you receive a new subscriber, they will appear here. </p>";
+		echo "<p>When you receive a new subscriber, they will appear here. </p>";
 
-    	}
+	}
 }
 
 ?>
